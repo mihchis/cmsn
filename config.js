de2a107287c17686b2633f884f72f81d2427efd3
mihchis <trin79136@gmail.com>
2021-08-24 23:27:10 +0700

    1
    1

---
+++ config.js
@@ -0,0 +1,16 @@
+const CONFIG = {
+    introTitle: 'Chúc mừng sinh nhật MINH NGHI cuteeeeeeeeeeeeeeeeeeeeeeeee!',
+    introDesc: `Còn nữa đừng có thoát`,
+    btnIntro: 'Bấm vào đây để coi tiếp nè !',
+    title: 'Mình đi papas ăn sn điiiiiiiii :>',
+    desc: 'Cmsn MN nhe hí hí, chúc MN tuổi mới luôn đẹp gái, học giỏi, hạnh phúc, làm việc khoa hay việc gì cũng phải thuận lợi nha, nhưng quan trọng là phải luôn vui vẻ đừng có quạo với đừng có thức khuya nữa. Loveee nhiuu <3 😙 ',
+    btnYes: 'oke :3',
+    btnNo: 'khum đi :)))',
+    question:'Hứa ở đây điiiiii!',
+    btnReply: 'Xong rùi đó :v',
+    reply: 'Tui mà ko đi ăn với mihchis là mihchis bỏ đi du học suốt đời luônnnnn !!!',
+    mess: 'Áhihi kiểu gì cũng phải đồng ý thoi :)))',
+    messDesc: 'Qua đây nhắn tin lên kèo nè :v',
+    btnAccept: 'Okiiiii lun :v',
+    messLink: 'https://m.me/mihchis'
+}
